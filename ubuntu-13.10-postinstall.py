2579b9aba81a2204b9bffc292dac64f78622470b
Shiquan Wang <shiquanwang@gmail.com>
2013-10-16 16:53:47 +0800

    Add: Add script header.

---
+++ ubuntu-13.10-postinstall.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# My script of Ubuntu 13.10 post installation
+#
+# Syntax: # sudo ./ubuntu-13.10-postinstall.py
+#
+# Shiquan Wang <shiquanwang@gmail.com>
+# http://shiquanwang.github.io
+# Distributed under the GPL version 3 license
+
+
+__appname__ = 'ubuntu-13.10-postinstall'
+__version__ = '0.1'
+__author__ = 'Shiquan Wang <shiquanwang@gmail.com>'
+__licence__ = 'LGPL'
+
+"""
+Post installation script for Ubuntu 13.10
+"""
